b7b3b95bb98960e41df5dd7aa5bb554a08287956
wangzhijie86 <53120767+wangzhijie86@users.noreply.github.com>
2020-07-13 11:27:03 +0800

    复习第一次提交

---
+++ day01.2_JavaDataType/src/com/itheima/DataType.java
@@ -0,0 +1,23 @@
+package com.itheima;
+
+public class DataType {
+    public static void main(String[] args) {
+        /**Java数据类型四类八种
+         *      1.基本数据类型
+         *          1.1数值型
+         *              *整型 byte，short,int,long
+         *              *浮点型 float,double
+         *              *字符型 char
+         *
+         *          1.2非数值型
+         *              1.布尔型(boolean) true,fa
+         *
+         *      2.引用数据类型
+         *          1.类(class)
+         *          2.接口(interface)
+         *          3.数组([])
+         *
+         */
+        // TODO: 2020/7/5 没有写完数据类型已经类型转换
+    }
+}
